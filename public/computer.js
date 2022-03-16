e7157db2c1169815cd17fed5a6874c30b49ab5b6
Jossl123 <jossl1.gau@gmail.com>
2022-03-16 15:32:44 +0100

    I think I have correct the bugs

--- public/computer.js
+++ public/computer.js
@@ -31,24 +31,33 @@ NOT = new Block([0], [0], "0!")
 
 function cutStr(str, debut, fin) { return str.substr(0, debut) + str.substr(fin) }
 
-function link(i1, n1, i2, iN) { //i1 : index block cliqué; n1 : longueur block cliqué; i2 : index block destination; iN : longueur block destination
-    console.log(i1, n1, i2, iN, current_road)
+function link(i1, n1, i2, iN) { //i1 : index block cliqué; n1 : longueur block cliqué; i2 : index block destination; iN : index de l'input du block
+    console.log(n1, iN)
     var index_debut_block = i1 - (n1 - 1) // index debut du block cliqué
     var block = current_road.substr(i1 - n1 + 1, n1) // string contenant le block a relier
     document.getElementById(`${i1}`).setAttribute("id", "waiting")
+    document.getElementById(`${i2}`).setAttribute("l", parseInt(document.getElementById(`${i2}`).getAttribute("l"))+n1-1)
     current_road = cutStr(current_road, index_debut_block, index_debut_block + n1) // on retire le block cliqué du chemin
     for (let i = i1; i < current_road.length + block.length; i++) { //on actualise l'id de tous les blocks après celui qui vient d'être retiré du chemin
         if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i - n1) }
     }
-    console.log(current_road)
     if (i1 < i2) { i2 -= n1 }
-    p = iN //todo: faire boucle iN fois pour chaque getelementid() et ajouter les getattribute("l")
-    if (current_road[i2 - p] == "K") { //verifie qu'il n'y a pas de case a compléter juste avant
-        current_road = current_road.slice(0, i2 - p) + block + current_road.slice(i2 - p + 1);
-        for (let i = i2 - p + 1; i < current_road.length + block.length; i++) {
+    var currentI = i2-1
+    for (let i = 1; i < iN; i++) {
+        console.log(currentI)
+        if (current_road[currentI]) {
+            currentI-=1
+        }else{
+            currentI-=document.getElementById(`${currentI}`).getAttribute("l")
+        }
+    }
+    //p = currentI //todo: faire boucle iN fois pour chaque getelementid() et ajouter les getattribute("l")
+    if (current_road[currentI] == "K") { //verifie qu'il n'y a pas de case a compléter juste avant
+        current_road = current_road.slice(0, currentI) + block + current_road.slice(currentI + 1);
+        for (let i = current_road.length + block.length -1; i >= currentI+1; i--) {
             if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i + block.length - 1) }
         }
-        document.getElementById("waiting").setAttribute("id", i2 - p)
+        document.getElementById("waiting").setAttribute("id", currentI)
     } else {
         current_road = current_road.slice(0, i2) + block + current_road.slice(i2);
         for (let i = i2; i < current_road.length + block.length; i++) {
@@ -74,6 +83,7 @@ function create_link(e) {
         point_to_link = 0
     } else {
         if (e.path[0].getAttribute('type') == "i") {
+            console.log(e.path[0].getAttribute("iN"))
             point_to_link = [e.path[0].getAttribute("type"), e.path[1].id, e.path[0].getAttribute("iN")]
         } else {
             point_to_link = [e.path[0].getAttribute("type"), e.path[1].id, e.path[1].getAttribute("l")]
