da1b0fd561538b32d4ced395bf1b5d99767f6df6
Jossl123 <jossl1.gau@gmail.com>
2022-03-16 14:29:18 +0100

    can't find where it crash

--- public/computerDraw.js
+++ public/computerDraw.js
@@ -20,14 +20,16 @@ function add_block(block) {
     var h = Math.max(block.inputs.length, block.outputs.length)
     var r = `<div id="${current_road.length+block.action.length-1}" l="${block.action.length}" ondrag="element_drag(event)" draggable="true" class="z-30 absolute top-1/2 left-1/2 bg-blue-600 w-20" style="height: ${h*2}rem">`
     for (let i = 0; i < block.inputs.length; i++) r += `<div iN="${i+1}" type="i" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -left-3" style="top: ${i*2+0.25}rem"></div>`
-    for (let i = 0; i < block.outputs.length; i++) r += `<div type="o" class="absolute bg-white rounded-full h-6 w-6 -right-3" style="top: ${i*2+0.25}rem"></div>`
-    r += `<p class="absolute" style="left: 1.25rem; top: ${h-0.75}rem; bottom: ${h-0.75}rem">${"AND"}</p></div>`
+    for (let i = 0; i < block.outputs.length; i++) r += `<div type="o" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -right-3" style="top: ${i*2+0.25}rem"></div>`
+    r += `<p class="absolute" style="left: 1.25rem; top: ${h-0.75}rem; bottom: ${h-0.75}rem">${prompt("nom")}</p></div>`
     document.getElementById("content").innerHTML += r
     current_road += block.action.replace(/[0-9]/g, 'K')
 }
 add_input_point()
 add_input_point()
 add_block(AND)
+add_block(NOT)
+add_block(OR)
 var element_dragged
 
 function element_drag(event) {
