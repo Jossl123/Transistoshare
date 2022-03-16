c8b43e7daf3faf664ebd75ca3cc0f58870bb8926
Jossl123 <jossl1.gau@gmail.com>
2022-03-16 14:05:09 +0100

    the computer seems to be working

--- public/computer.js
+++ public/computer.js
@@ -1,5 +1,7 @@
-var current_road = "" //chaque charactere aura deux valeurs : son index dans la chaine et le nb de lettre qui sont accrochés derriere lui
-var inputs_nb = 0
+var current_road = ""; //chaque charactere aura deux valeurs : son index dans la chaine et le nb de lettre qui sont accrochés derriere lui
+var inputs_nb = 0;
+var linking = false;
+var point_to_link;
 class Block {
     constructor(inputs, outputs, action) {
         this.inputs = inputs
@@ -29,40 +31,42 @@ NOT = new Block([0], [0], "0!")
 
 function cutStr(str, debut, fin) { return str.substr(0, debut) + str.substr(fin) }
 
-function link(i1, n1, i2, iN) {
-    console.log(i1, n1, i2, iN)
-    var index_debut_block = i1 - (n1 - 1)
-    var block = current_road.substr(i1 - n1 + 1, n1)
-    current_road = cutStr(current_road, index_debut_block, index_debut_block + n1)
+function link(i1, n1, i2, iN) { //i1 : index block cliqué; n1 : longueur block cliqué; i2 : index block destination; iN : longueur block destination
+    console.log(i1, n1, i2, iN, current_road)
+    var index_debut_block = i1 - (n1 - 1) // index debut du block cliqué
+    var block = current_road.substr(i1 - n1 + 1, n1) // string contenant le block a relier
+    document.getElementById(`${i1}`).setAttribute("id", "waiting")
+    current_road = cutStr(current_road, index_debut_block, index_debut_block + n1) // on retire le block cliqué du chemin
+    for (let i = i1; i < current_road.length + block.length; i++) { //on actualise l'id de tous les blocks après celui qui vient d'être retiré du chemin
+        if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i - n1) }
+    }
+    console.log(current_road)
     if (i1 < i2) { i2 -= n1 }
-    p = iN //faire boucle iN fois pour chaque getelementid() et ajouter les getattribute("l")
+    p = iN //todo: faire boucle iN fois pour chaque getelementid() et ajouter les getattribute("l")
     if (current_road[i2 - p] == "K") { //verifie qu'il n'y a pas de case a compléter juste avant
         current_road = current_road.slice(0, i2 - p) + block + current_road.slice(i2 - p + 1);
         for (let i = i2 - p + 1; i < current_road.length + block.length; i++) {
-            if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i - 1) }
+            if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i + block.length - 1) }
         }
+        document.getElementById("waiting").setAttribute("id", i2 - p)
     } else {
         current_road = current_road.slice(0, i2) + block + current_road.slice(i2);
         for (let i = i2; i < current_road.length + block.length; i++) {
             if (document.getElementById(i)) { document.getElementById(i).setAttribute("id", i - 1) }
         }
+        document.getElementById("waiting").setAttribute("id", i2)
     }
     console.log(current_road)
 }
-add_input_point()
-add_input_point()
-add_block(AND)
-var linking = false
-var point_to_link
 
 function create_link(e) {
-    if (linking) {
-        if (e.path[0].getAttribute("type") != point_to_link[0]) {
-            if (point_to_link[0] == "o") {
-                console.log("o")
+    if (linking) { //si on a deja selectionné un point
+        if (e.path[0].getAttribute("type") != point_to_link[0]) { //si le point cliqué n'est pas du même type que le premier point cliqué (intput / output)
+            if (point_to_link[0] == "o") { //si le premier point était un output
+                console.log("o", point_to_link)
                 link(parseInt(point_to_link[1]), parseInt(point_to_link[2]), parseInt(e.path[1].id), parseInt(e.path[0].getAttribute("iN")))
-            } else {
-                console.log("e", point_to_link)
+            } else { //si le premier point était un input
+                console.log("i", point_to_link)
                 link(parseInt(e.path[1].id), parseInt(e.path[1].getAttribute("l")), parseInt(point_to_link[1]), parseInt(point_to_link[2]))
             }
         }
--- public/computerDraw.js
+++ public/computerDraw.js
@@ -21,7 +21,33 @@ function add_block(block) {
     var r = `<div id="${current_road.length+block.action.length-1}" l="${block.action.length}" ondrag="element_drag(event)" draggable="true" class="z-30 absolute top-1/2 left-1/2 bg-blue-600 w-20" style="height: ${h*2}rem">`
     for (let i = 0; i < block.inputs.length; i++) r += `<div iN="${i+1}" type="i" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -left-3" style="top: ${i*2+0.25}rem"></div>`
     for (let i = 0; i < block.outputs.length; i++) r += `<div type="o" class="absolute bg-white rounded-full h-6 w-6 -right-3" style="top: ${i*2+0.25}rem"></div>`
-    r += `<p class="absolute" style="left: 1.25rem; top: ${h-0.75}rem; bottom: ${h-0.75}rem">${"test"}</p></div>`
+    r += `<p class="absolute" style="left: 1.25rem; top: ${h-0.75}rem; bottom: ${h-0.75}rem">${"AND"}</p></div>`
     document.getElementById("content").innerHTML += r
     current_road += block.action.replace(/[0-9]/g, 'K')
+}
+add_input_point()
+add_input_point()
+add_block(AND)
+var element_dragged
+
+function element_drag(event) {
+    element_dragged = event.path[0].id
+}
+
+function drop(event) {
+    if (event.path[0].id == "button_block") {
+        delete_block(element_dragged)
+    } else {
+        var dm = document.getElementById(element_dragged);
+        dm.style.left = event.clientX - 60 + 'px';
+        dm.style.top = event.clientY - 30 + 'px';
+    }
+    event.preventDefault();
+    update_joint()
+    return false;
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
 }
