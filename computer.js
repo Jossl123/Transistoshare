cbff7d4a76072ab98da5f0ec2249bd2212d4faa4
Jossl123 <jossl1.gau@gmail.com>
2022-05-04 14:25:20 +0200

    allow online usage

--- computer.js
+++ computer.js
@@ -5,6 +5,10 @@ String.prototype.replaceAt = function(index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
 
+//----------------------------------------------------------------
+// fonctions pour gérer la notation polonaise
+//----------------------------------------------------------------
+
 function executeById(id) {
     /**
      execute l'action du transistor a partir de son id
@@ -108,83 +112,6 @@ function unconvertToN(a) {
     return a
 }
 
-function update() {
-    /**
-     permet d'actualiser la couleur de des fils et des inputs / outputs
-     */
-    for (let i = 0; i < links.length; i++) {
-        var v = executeById(document.getElementById(links[i][0]).id)
-        document.getElementById(links[i][0]).setAttribute("value", v)
-        document.getElementById(links[i][1]).setAttribute("value", v)
-        if (v == 1) {
-            document.getElementById(`line-${links[i][0]}-${links[i][1]}`).style.stroke = "rgb(240, 60, 60)"
-            document.getElementById(links[i][0]).style.backgroundColor = "rgb(240, 60, 60)"
-            document.getElementById(links[i][1]).style.backgroundColor = "rgb(240, 60, 60)"
-            if (links[i][1][0] == "O") document.getElementById(links[i][1] + "_o").style.backgroundColor = "rgb(240, 60, 60)"
-        } else {
-            document.getElementById(`line-${links[i][0]}-${links[i][1]}`).style.stroke = "rgb(255, 255, 255)"
-            document.getElementById(links[i][0]).style.backgroundColor = "rgb(255, 255, 255)"
-            document.getElementById(links[i][1]).style.backgroundColor = "rgb(255, 255, 255)"
-            if (links[i][1][0] == "O") document.getElementById(links[i][1] + "_o").style.backgroundColor = "rgb(255, 255, 255)"
-        }
-    }
-}
-
-function create_link(e) {
-    /**
-     permet de construire un fil entre deux points
-     */
-    if (linking) { //si on a deja selectionné un point
-        try {
-            if (e.path[0].getAttribute("type") != document.getElementById(point_to_link).getAttribute("type")) { //si le point cliqué n'est pas du même type que le premier point cliqué (intput / output)
-                var pos1 = document.getElementById(point_to_link).getBoundingClientRect()
-                var pos2 = e.path[0].getBoundingClientRect()
-                if (document.getElementById(point_to_link).getAttribute("type") == "o") { //si le premier point était un output
-                    links.push([point_to_link, e.path[0].id])
-                    document.getElementById("svg_joint").innerHTML += `<line id="line-${point_to_link}-${e.path[0].id}" onclick="delete_joint(event)" class="z-30" value="0" id="j" x1="${pos1.left}" y1="${pos1.top}" x2="${pos2.left}" y2="${pos2.top}" style="stroke:rgb(255, 255, 255);stroke-width:4" />`
-                } else { //si le premier point était un input
-                    links.push([e.path[0].id, point_to_link])
-                    document.getElementById("svg_joint").innerHTML += `<line id="line-${e.path[0].id}-${point_to_link}" onclick="delete_joint(event)" class="z-30" value="0" id="j" x1="${pos1.left}" y1="${pos1.top}" x2="${pos2.left}" y2="${pos2.top}" style="stroke:rgb(255, 255, 255);stroke-width:4" />`
-                }
-                linking = false
-                link_nb++
-                point_to_link = 0
-                document.body.style.cursor = "pointer"
-            }
-        } catch (error) {
-            console.log(error)
-            linking = false
-            point_to_link = 0
-            document.body.style.cursor = "pointer"
-        }
-
-    } else {
-        document.body.style.cursor = "crosshair"
-        point_to_link = e.path[0].getAttribute("id")
-        linking = true
-    }
-    update()
-}
-
-function create_block() {
-    /**
-     permet de construire un nouveau transistor a partir du circuit actuel de l'utilisateur
-     */
-    var outputsActions = []
-    var outputsConnectedId = []
-    for (let i = 0; i < links.length; i++) {
-        if (links[i][1].includes("O_")) {
-            outputsConnectedId.push(links[i][1].split("_")[1])
-        }
-    }
-    outputsConnectedId.sort((a, b) => a - b);
-    for (let i = 0; i < outputsConnectedId.length; i++) {
-        outputsActions.push(trace_path(`O_${outputsConnectedId[i]}`))
-    }
-    var block_name = prompt("Name of the bloc : ")
-    add_transistor(outputsActions, block_name)
-    document.getElementById("button_block").innerHTML += `<button onclick='add_transistor(${JSON.stringify(outputsActions)}, "${block_name}")' class="m-2 text-white">${block_name}</button>`
-}
 
 setInterval(function() { //horloge qui actualise toutes les 0.1 secondes
     update()
--- computerDraw.js
+++ computerDraw.js
@@ -1,33 +1,36 @@
+var user_transistors = [
+    [
+        ['0.1.&'], 'AND'
+    ],
+    [
+        ['0.1.|'], 'OR'
+    ],
+    [
+        ['0.!'], 'NOT'
+    ],
+    [
+        ['0.1.|.1.0.&.!.&.2.|.2.0.1.|.1.0.&.!.&.&.!.&', '0.1.|.1.0.&.!.&.2.&.1.0.&.|'], 'ADDER'
+    ],
+    [
+        ['0.4.|.4.0.&.!.&.1.5.|.5.1.&.!.&.2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.&.6.2.&.|.&.5.1.&.|.|.1.5.|.5.1.&.!.&.2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.&.6.2.&.|.&.5.1.&.|.0.4.|.4.0.&.!.&.&.!.&', '1.5.|.5.1.&.!.&.2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.&.6.2.&.|.|.2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.&.6.2.&.|.1.5.|.5.1.&.!.&.&.!.&', '2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.|.3.7.|.7.3.&.!.&.8.&.7.3.&.|.2.6.|.6.2.&.!.&.&.!.&', '3.7.|.7.3.&.!.&.8.|.8.3.7.|.7.3.&.!.&.&.!.&', '0.4.|.4.0.&.!.&.1.5.|.5.1.&.!.&.2.6.|.6.2.&.!.&.3.7.|.7.3.&.!.&.8.&.7.3.&.|.&.6.2.&.|.&.5.1.&.|.&.4.0.&.|'], '4bitAdder'
+    ],
+    [
+        ['0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.|.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.&.!.&', '1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.|.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.&.!.&', '2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.|.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.&.!.&', '3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.|.8.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.&.!.&', '0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.&.4.8.|.8.4.&.!.&.0.&.|', '0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.|.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.&.!.&', '0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.|.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.&.5.8.|.8.5.&.!.&.1.&.|.0.4.8.|.8.4.&.!.&.|.4.8.|.8.4.&.!.&.0.&.!.&.&.!.&.!.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.|.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.&.6.8.|.8.6.&.!.&.2.&.|.1.5.8.|.8.5.&.!.&.|.5.8.|.8.5.&.!.&.1.&.!.&.&.!.&.!.&.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.|.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.&.7.8.|.8.7.&.!.&.3.&.|.2.6.8.|.8.6.&.!.&.|.6.8.|.8.6.&.!.&.2.&.!.&.&.!.&.!.&.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.8.|.8.3.7.8.|.8.7.&.!.&.|.7.8.|.8.7.&.!.&.3.&.!.&.&.!.&.!.&'], 'ALU'
+    ]
+]
 var transistors_nb = 0
 var input_nb = 0
 var output_nb = 0
 var element_dragged
 
-function add_input_point() {
-    /**
-     ajoute un input sur la bare grise a gauche
-     */
-    document.getElementById("inputs_points").innerHTML += `
-    <div id="I_${input_nb}" l="1" name="" action="${input_nb}" class="z-20 flex inline-flex items-center">
-        <button id="I_${input_nb}_i" value="0" onclick="change_input_value('I_${input_nb}_i')" class="h-8 w-8 my-1 z-20 focus:outline-none rounded-full bg-white"></button>
-        <button id="I_${input_nb}_o" type="o" action="${input_nb}" onclick="create_link(event)" class="h-4 w-4 z-20 focus:outline-none rounded-full" style="background-color:rgb(255, 255, 255)"></button>
-    </div>`
-    input_nb++
-    update_joint()
+function init_userTransistors() {
+    // permet d'écrire les transistors de l'utilisateur dynamiquement
+    document.getElementById("transistors_block").innerHTML = ""
+    for (let i = 0; i < user_transistors.length; i++) {
+        document.getElementById("transistors_block").innerHTML += `<button class="px-2 py-1 rounded transition hover:bg-white/25 text-white" onclick='add_transistor(${JSON.stringify(user_transistors[i][0])}, "${user_transistors[i][1]}")'>${user_transistors[i][1]}</button>`
+    }
 }
-
-function add_output_point() {
-    /**
-     ajoute un output sur la bare grise a droite
-     */
-    document.getElementById("outputs_points").innerHTML += `
-    <div action="${output_nb}" class="z-20 flex inline-flex items-center">
-        <button id="O_${output_nb}" type="i" action="${output_nb}" onclick="create_link(event)" class="h-4 w-4 z-20 focus:outline-none rounded-full bg-white"></button>
-        <button id="O_${output_nb}_o" value="0" class="h-8 w-8 my-1 z-20 focus:outline-none rounded-full" style="background-color:rgb(255, 255, 255)"></button>
-    </div>`
-    output_nb++
-    update_joint()
-}
+init_userTransistors()
 
 function add_transistor(outputsActions, name) {
     /**
@@ -40,13 +43,37 @@ function add_transistor(outputsActions, name) {
     }
     inputNb++
     var h = Math.max(inputNb, outputsActions.length)
-    var r = `<div id="${transistors_nb}"onMouseOut="this.style.boxShadow='0 0 0 0 rgba(200, 200, 200, .3)'" onMouseOver="this.style.boxShadow='0 0 0 9px rgba(200, 200, 200, .3)'" ondrag="element_drag(event)" ondblclick="delete_transistor(this)" draggable="true" class="z-30 absolute top-1/2 left-1/2 inline px-4" style="background-color:${stringToColor(name)};height: ${h*2}rem">`
-    for (let i = 0; i < inputNb; i++) r += `<div id="${transistors_nb}_i_${i}" type="i" value="0" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -left-3" style="top: ${i*2+0.25}rem"></div>`
-    for (let i = 0; i < outputsActions.length; i++) r += `<div id="${transistors_nb}_o_${i}" action="${outputsActions[i]}" type="o" value="0" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -right-3" style="top: ${i*2+0.25}rem"></div>`
-    r += `<p>${name}</p></div>`
-    document.getElementById("content").innerHTML += r
+
+    var newElem = document.createElement('button')
+    const elemAttrs = {
+        id: transistors_nb,
+        onMouseOut: "this.style.boxShadow='0 0 0 5px rgba(200, 200, 200, 0)'",
+        onMouseOver: "this.style.boxShadow='0 0 0 5px rgba(200, 200, 200, .25)'",
+        ondrag: "element_drag(event)",
+        ondblclick: "delete_transistor(this)",
+        draggable: true,
+        class: "rounded z-30 absolute top-1/2 left-1/2 inline px-4 transition-opacity transition-transform duration-200 scale-90 opacity-0",
+        style: `background-color:${stringToColor(name)};height: ${h*2}rem`
+    }
+
+    for (const [attr, attrValue] of Object.entries(elemAttrs)) {
+        newElem.setAttribute(attr, attrValue)
+    }
+
+    for (let i = 0; i < inputNb; i++) newElem.innerHTML += `<div id="${transistors_nb}_i_${i}" type="i" value="0" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -left-3" style="top: ${i*2+0.25}rem"></div>`
+    for (let i = 0; i < outputsActions.length; i++) newElem.innerHTML += `<div id="${transistors_nb}_o_${i}" action="${outputsActions[i]}" type="o" value="0" onclick="create_link(event)" class="absolute bg-white rounded-full h-6 w-6 -right-3" style="top: ${i*2+0.25}rem"></div>`
+    newElem.innerHTML += `<p class="text-white mix-blend-luminosity">${name}</p>`
+
+    document.getElementById("content").appendChild(newElem)
     transistors_nb++
     update_joint()
+
+    // Transition
+    requestAnimationFrame(() =>
+        setTimeout(() => {
+            document.getElementById(elemAttrs.id).classList.remove('scale-90', 'opacity-0')
+        })
+    )
 }
 
 function delete_transistor(el) {
@@ -64,6 +91,43 @@ function delete_transistor(el) {
     update_joint()
 }
 
+function add_input_point() {
+    /**
+     ajoute un input sur la bare grise a gauche
+     */
+    document.getElementById("inputs_points").innerHTML += `
+    <div id="I_${input_nb}" l="1" name="" action="${input_nb}" class="z-20 flex inline-flex items-center">
+        <button id="I_${input_nb}_i" value="0" onclick="change_input_value('I_${input_nb}_i')" class="h-8 w-8 my-1 z-20 focus:outline-none rounded-full bg-white"></button>
+        <button id="I_${input_nb}_o" type="o" action="${input_nb}" onclick="create_link(event)" class="h-4 w-4 z-20 focus:outline-none rounded-full" style="background-color:rgb(255, 255, 255)"></button>
+    </div>`
+    input_nb++
+    update_joint()
+}
+
+function change_input_value(id) {
+    /**
+    change la valeur des inputs sur la barre de gauche 
+     */
+    v = document.getElementById(id).getAttribute("value")
+    document.getElementById(id).setAttribute("value", Number(!parseInt(v)))
+    if (v == 0) document.getElementById(id).style.backgroundColor = "rgb(240, 60, 60)"
+    else document.getElementById(id).style.backgroundColor = "rgb(255, 255, 255)"
+
+}
+
+function add_output_point() {
+    /**
+     ajoute un output sur la bare grise a droite
+     */
+    document.getElementById("outputs_points").innerHTML += `
+    <div action="${output_nb}" class="z-20 flex inline-flex items-center">
+        <button id="O_${output_nb}" type="i" action="${output_nb}" onclick="create_link(event)" class="h-4 w-4 z-20 focus:outline-none rounded-full bg-white"></button>
+        <button id="O_${output_nb}_o" value="0" class="h-8 w-8 my-1 z-20 focus:outline-none rounded-full" style="background-color:rgb(255, 255, 255)"></button>
+    </div>`
+    output_nb++
+    update_joint()
+}
+
 function stringToColor(stringInput) {
     /**
      retourne une couleur en fonction d'une chaine de caractères
@@ -74,25 +138,87 @@ function stringToColor(stringInput) {
     return `hsl(${stringUniqueHash % 360}, 95%, 35%)`;
 }
 
-/**
- element_drag, drop et drag_over permettent de pouvoir deplacer les transistors
- */
-function element_drag(event) {
-    element_dragged = event.path[0].id
+function update() {
+    /**
+     permet d'actualiser la couleur de des fils et des inputs / outputs
+     */
+    for (let i = 0; i < links.length; i++) {
+        var v = executeById(document.getElementById(links[i][0]).id)
+        document.getElementById(links[i][0]).setAttribute("value", v)
+        document.getElementById(links[i][1]).setAttribute("value", v)
+        if (v == 1) {
+            document.getElementById(`line-${links[i][0]}-${links[i][1]}`).style.stroke = "rgb(240, 60, 60)"
+            document.getElementById(links[i][0]).style.backgroundColor = "rgb(240, 60, 60)"
+            document.getElementById(links[i][1]).style.backgroundColor = "rgb(240, 60, 60)"
+            if (links[i][1][0] == "O") document.getElementById(links[i][1] + "_o").style.backgroundColor = "rgb(240, 60, 60)"
+        } else {
+            document.getElementById(`line-${links[i][0]}-${links[i][1]}`).style.stroke = "rgb(255, 255, 255)"
+            document.getElementById(links[i][0]).style.backgroundColor = "rgb(255, 255, 255)"
+            document.getElementById(links[i][1]).style.backgroundColor = "rgb(255, 255, 255)"
+            if (links[i][1][0] == "O") document.getElementById(links[i][1] + "_o").style.backgroundColor = "rgb(255, 255, 255)"
+        }
+    }
 }
 
-function drop(event) {
-    var dm = document.getElementById(element_dragged);
-    dm.style.left = event.clientX - 60 + 'px';
-    dm.style.top = event.clientY - 30 + 'px';
-    update_joint()
-    event.preventDefault();
-    return false;
+function create_block() {
+    /**
+     permet de construire un nouveau transistor a partir du circuit actuel de l'utilisateur
+     */
+    var outputsActions = []
+    var outputsConnectedId = []
+    for (let i = 0; i < links.length; i++) {
+        if (links[i][1].includes("O_")) {
+            outputsConnectedId.push(links[i][1].split("_")[1])
+        }
+    }
+    outputsConnectedId.sort((a, b) => a - b);
+    for (let i = 0; i < outputsConnectedId.length; i++) {
+        outputsActions.push(trace_path(`O_${outputsConnectedId[i]}`))
+    }
+    var block_name = prompt("Name of the bloc : ")
+    user_transistors.push([outputsActions, block_name])
+    init_userTransistors()
+    add_transistor(outputsActions, block_name)
 }
 
-function drag_over(event) {
-    event.preventDefault();
-    return false;
+//-------------------------------------------------------------------------------------
+// fonctions qui gèrent les fils
+//-------------------------------------------------------------------------------------
+
+function create_link(e) {
+    /**
+     permet de construire un fil entre deux points
+     */
+    if (linking) { //si on a deja selectionné un point
+        try {
+            if (e.path[0].getAttribute("type") != document.getElementById(point_to_link).getAttribute("type")) { //si le point cliqué n'est pas du même type que le premier point cliqué (intput / output)
+                var pos1 = document.getElementById(point_to_link).getBoundingClientRect()
+                var pos2 = e.path[0].getBoundingClientRect()
+                if (document.getElementById(point_to_link).getAttribute("type") == "o") { //si le premier point était un output
+                    links.push([point_to_link, e.path[0].id])
+                    document.getElementById("svg_joint").innerHTML += `<line id="line-${point_to_link}-${e.path[0].id}" onclick="delete_joint(event)" class="z-30" value="0" id="j" x1="${pos1.left}" y1="${pos1.top}" x2="${pos2.left}" y2="${pos2.top}" style="stroke:rgb(255, 255, 255);stroke-width:4" />`
+                } else { //si le premier point était un input
+                    links.push([e.path[0].id, point_to_link])
+                    document.getElementById("svg_joint").innerHTML += `<line id="line-${e.path[0].id}-${point_to_link}" onclick="delete_joint(event)" class="z-30" value="0" id="j" x1="${pos1.left}" y1="${pos1.top}" x2="${pos2.left}" y2="${pos2.top}" style="stroke:rgb(255, 255, 255);stroke-width:4" />`
+                }
+                linking = false
+                link_nb++
+                point_to_link = 0
+                document.body.style.cursor = "pointer"
+            }
+        } catch (error) {
+            console.log(error)
+            linking = false
+            point_to_link = 0
+            document.body.style.cursor = "pointer"
+        }
+
+    } else {
+        document.body.style.cursor = "crosshair"
+        point_to_link = e.path[0].getAttribute("id")
+        linking = true
+    }
+    update()
 }
 
 function update_joint() {
@@ -123,15 +249,26 @@ function delete_joint(e) {
     update()
 }
 
-function change_input_value(id) {
-    /**
-    change la valeur des inputs sur la barre de gauche 
-     */
-    v = document.getElementById(id).getAttribute("value")
-    document.getElementById(id).setAttribute("value", Number(!parseInt(v)))
-    if (v == 0) document.getElementById(id).style.backgroundColor = "rgb(240, 60, 60)"
-    else document.getElementById(id).style.backgroundColor = "rgb(255, 255, 255)"
+window.addEventListener("resize", update_joint) //actualise les fils quand la fenetre est recadrer
 
+//-------------------------------------------------------------------------------------
+// fonctions qui permettent de pouvoir deplacer les transistors
+//-------------------------------------------------------------------------------------
+
+function element_drag(event) {
+    element_dragged = event.path[0].id
 }
 
-window.addEventListener("resize", update_joint) //actualise les fils quand la fenetre est recadrer
+function drop(event) {
+    var dm = document.getElementById(element_dragged);
+    dm.style.left = event.clientX - 60 + 'px';
+    dm.style.top = event.clientY - 30 + 'px';
+    update_joint()
+    event.preventDefault();
+    return false;
+}
+
+function drag_over(event) {
+    event.preventDefault();
+    return false;
+}
