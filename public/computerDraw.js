84715243a7c56fdcf6aa77e9f5b75dd5e745d6ca
Rémi CHARTIER <rchartier33127@gmail.com>
2022-05-05 17:40:20 +0200

    computer: Fix drag bugs

--- public/computerDraw.js
+++ public/computerDraw.js
@@ -274,8 +274,13 @@ function element_drag_ghost(event) {
 
 function drop(event) {
     var dm = document.getElementById(element_dragged)
-    dm.style.left = event.clientX - 60 + 'px'
-    dm.style.top = event.clientY - 30 + 'px'
+    console.log(event.clientX)
+    
+    if (event.clientX != 0 || event.clientY != 0) {
+        dm.style.left = event.clientX - 60 + 'px'
+        dm.style.top = event.clientY - 30 + 'px'
+    }
+
     update_joint()
     event.preventDefault()
     return false
